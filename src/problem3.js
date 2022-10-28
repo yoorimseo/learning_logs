b12ce4da65c28f57b09626797f7c3cf8c76f9513
yoorimseo <seou1225@naver.com>
2022-10-29 00:08:03 +0900

    feat: problem3 > number가 369를 포함하는지 확인하는 check369 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+// number가 369를 포함하는지 확인하는 함수
+function check369(str) {
+  // str이 3, 6, 9를 포함하고 있는지 확인하여 true 또는 false 반환
+  return str.includes("3") || str.includes("6") || str.includes("9");
+}
+
 function problem3(number) {
   var answer;
   return answer;
