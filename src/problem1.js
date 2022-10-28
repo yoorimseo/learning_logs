59cad377a28b31b2fb8e72144e43ec0df0e72f55
yoorimseo <seou1225@naver.com>
2022-10-28 15:13:56 +0900

    feat: problem1 > 각 자리수의 곱을 구하는 multiplyOfDigits 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -15,6 +15,16 @@ function sumOfDigits(num) {
   return result;
 }
 
+// 각 자리의 곱을 구하는 함수
+function multiplyOfDigits(num) {
+  let arr = num
+    .toString()
+    .split("")
+    .map((s) => parseInt(s));
+  let result = arr.reduce((acc, cur) => acc * cur, 1);
+  return result;
+}
+
 function problem1(pobi, crong) {
   // 예외처리 1) 펼친 페이지가 연속된 페이지가 아닐 경우
   if (exception(pobi) || exception(crong)) {
