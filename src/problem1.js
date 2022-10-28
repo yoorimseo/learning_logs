dc94df6ed5ef5eb43172b6c1013f981fd57bbfaf
yoorimseo <seou1225@naver.com>
2022-10-28 15:10:24 +0900

    feat: problem1 > 예외처리를 위한 exception 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
+// 예외처리 1) 펼친 페이지가 연속된 페이지가 아닐 경우
+function exception(array) {
+  if (array[0] + 1 === array[1]) {
+    return true;
+  }
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  // 예외처리 1) 펼친 페이지가 연속된 페이지가 아닐 경우
+  if (exception(pobi) || exception(crong)) {
+    return -1;
+  }
 }
 
 module.exports = problem1;
