decdbbc412ba3075518faefe42e4d977ffe7cd65
yoorimseo <seou1225@naver.com>
2022-10-29 00:09:08 +0900

    feat: problem3 > 1부터 number까지 369를 몇 개 포함하는지 구하는 for문 추가

--- src/problem3.js
+++ src/problem3.js
@@ -5,7 +5,27 @@ function check369(str) {
 }
 
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  // 1부터 number까지 369를 몇 개 포함하는지 구하는 for문
+  for (let i = 1; i <= number; i++) {
+    if (i < 30) {
+      // 1 <= i <= 29일 때, 369를 한 개씩만 가지므로 다음의 조건문 실행
+      let numToStr = i.toString();
+      if (check369(numToStr)) {
+        console.log(i);
+        answer += 1;
+      }
+    } else {
+      // i <= 30일 때, 369를 한개 이상 가질 수 있으므로 다음의 조건문 실행
+      let numToArr = i.toString().split("");
+      console.log(numToArr);
+      // numToArr가 몇 개의 369를 가졌는지 확인
+      let include369 = numToArr.filter((num) => check369(num));
+      answer += include369.length;
+    }
+  }
+
   return answer;
 }
 
