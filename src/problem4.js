5a0479a2d64c150a3f60cfa5b1722cfb793bacb4
yoorimseo <seou1225@naver.com>
2022-10-29 22:34:18 +0900

    feat: problem4 > 알파벳과 거꾸로 알파벳을 담은 배열 alphabet, reverseAlphabet 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,11 @@
 function problem4(word) {
   var answer;
+
+  // 원래의 알파벳 배열
+  let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // 순서를 뒤집은 알파벳 배열
+  let reverseAlphabet = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"];
+
   return answer;
 }
 
