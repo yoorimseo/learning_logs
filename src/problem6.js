1a5b4ca6f2984a1a0ac2a87c293ebd1d589a4bc0
yoorimseo <seou1225@naver.com>
2022-10-31 00:07:51 +0900

    feat: problem6 > 닉네임을 연속된 두 글자로 나누어 배열로 반환하는 sliceTwoWords 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,15 @@
+// 닉네임을 연속된 두 글자로 나누어 배열로 반환하는 함수
+function sliceTwoWords(str) {
+  let twoWords = [];
+
+  for (let i = 0; i < str.length - 1; i++) {
+    let sliceStr = str.slice(i, i + 2);
+    twoWords.push(sliceStr);
+  }
+
+  return twoWords;
+}
+
 function problem6(forms) {
   let formsObj = Object.fromEntries(forms);
   let emails = Object.keys(formsObj);
