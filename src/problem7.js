5951179c2565319d68b4ff53f8d6c3378d059876
yoorimseo <seou1225@naver.com>
2022-10-31 23:23:05 +0900

    feat: problem7 > friends 배열에서 user의 친구 찾는 findUserFriends 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,20 @@
+// friends 배열에서 user의 친구 찾기
+function findUserFriends(user, friends) {
+  let userFriends = [];
+
+  for (i of friends) {
+    if (i.includes(user)) {
+      if (i.indexOf(user) === 0) {
+        userFriends.push(i[1]);
+      } else {
+        userFriends.push(i[0]);
+      }
+    }
+  }
+
+  return userFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
