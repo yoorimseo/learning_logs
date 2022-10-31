ce3590f385c9b1e4f88338de1ccc24d8df1a67ce
yoorimseo <seou1225@naver.com>
2022-10-31 23:24:00 +0900

    feat: problem7 > neighbor에서 추천 친구 찾는 recommendsInNeihbor 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -35,6 +35,24 @@ function findUserNeighbor(userFriends, friends, user) {
   return neighbor;
 }
 
+// neighbor에서 추천 친구 찾기
+// userFriends의 친구인 neighbor가 0명 이하일 땐 추천하지 않음
+function recommendsInNeihbor(neighbor, recommends) {
+  if (neighbor.length > 0) {
+    neighbor.forEach((n) => {
+      if (recommends[n]) {
+        // recommends에 있으면 아이디에 점수 값 추가
+        recommends[n] = recommends[n] + 10;
+      } else {
+        // recommends에 없으면 아이디와 점수 값 추가
+        recommends[n] = 0 + 10;
+      }
+    });
+  }
+
+  return recommends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
