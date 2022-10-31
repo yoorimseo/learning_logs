47b225cd927b2726594473d07de2f4cf4e35ad0d
yoorimseo <seou1225@naver.com>
2022-10-31 23:20:13 +0900

    refactor: problem3 > 배열 메소드를 사용하여 결과값 반환

--- src/problem3.js
+++ src/problem3.js
@@ -1,30 +1,8 @@
-// number가 369를 포함하는지 확인하는 함수
-function check369(str) {
-  // str이 3, 6, 9를 포함하고 있는지 확인하여 true 또는 false 반환
-  return str.includes("3") || str.includes("6") || str.includes("9");
-}
-
 function problem3(number) {
-  let answer = 0;
-
-  // 1부터 number까지 369를 몇 개 포함하는지 구하는 for문
-  for (let i = 1; i <= number; i++) {
-    if (i < 30) {
-      // 1 <= i <= 29일 때, 369를 한 개씩만 가지므로 다음의 조건문 실행
-      let numToStr = i.toString();
-      if (check369(numToStr)) {
-        console.log(i);
-        answer += 1;
-      }
-    } else {
-      // i <= 30일 때, 369를 한개 이상 가질 수 있으므로 다음의 조건문 실행
-      let numToArr = i.toString().split("");
-      console.log(numToArr);
-      // numToArr가 몇 개의 369를 가졌는지 확인
-      let include369 = numToArr.filter((num) => check369(num));
-      answer += include369.length;
-    }
-  }
+  let answer = [...Array(number)]
+    .map((_, i) => i + 1)
+    .toString()
+    .match(/3|6|9/g).length;
 
   return answer;
 }
