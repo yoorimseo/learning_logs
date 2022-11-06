2aeb6ddf0d5d838f8a6772fde563bf00d80cba58
yoorimseo <seou1225@naver.com>
2022-11-06 23:47:55 +0900

    feat(App): 3스트라이크 후, 게임을 새로 시작하거나 종료할 수 있는 기능 추가

--- src/App.js
+++ src/App.js
@@ -72,6 +72,14 @@ class App {
       }
     }
   }
+
+  restartGame() {
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (answer) => {
+      if (answer === '1') {
+        this.play();
+      }
+    });
+  }
 }
 
 module.exports = App;
