412d405b095b8ee075ef3814c17add68d6d52463
yoorimseo <seou1225@naver.com>
2022-11-07 01:14:14 +0900

    refactor(constructor): computer 초기값 설정 코드 삭제

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
-    this.computer = this.pickRandomNumber();
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
