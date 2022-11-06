0871815f6cc05b9817c29db8dc67966abb537de4
yoorimseo <seou1225@naver.com>
2022-11-06 23:46:14 +0900

    feat(App): 컴퓨터가 생각한 값과 사용자 입력값을 비교하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -16,7 +16,27 @@ class App {
   userInput(computer) {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       let userAnswer = answer.split('').map((str) => parseInt(str));
+      let result = this.compareOfValues(userAnswer, computer);
     });
+  }
+
+  compareOfValues(userAnswer, computer) {
+    let result = {
+      strike: 0,
+      ball: 0,
+    };
+
+    userAnswer.forEach((num) => {
+      if (computer.includes(num)) {
+        if (userAnswer.indexOf(num) === computer.indexOf(num)) {
+          result.strike += 1;
+        } else {
+          result.ball += 1;
+        }
+      }
+    });
+
+    return result;
   }
 }
 
