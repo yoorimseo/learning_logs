7287b8d5f9d0aaa1a18c4f941e0ccb5e49d7babc
yoorimseo <seou1225@naver.com>
2022-11-06 23:44:27 +0900

    feat(App): 게임 시작 문구를 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
     this.computer = this.pickRandomNumber();
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
   play() {}
