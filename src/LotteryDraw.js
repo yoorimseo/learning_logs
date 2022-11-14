bec1b8628ca9ac111eb9476d0e7526487db4b32d
yoorimseo <seou1225@naver.com>
2022-11-15 08:31:23 +0900

    refactor(LotteryDraw): 로또의 결과를 보여주는 기능 추가

--- src/LotteryDraw.js
+++ src/LotteryDraw.js
@@ -1,19 +1,37 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+const LottoResult = require('./LottoResult');
+const lottoResult = new LottoResult();
+
+const WINNING_NUMBERS = {
+  winningNumber: [],
+  bonusNumer: 0,
+};
+
+const WINNING_HISTORY = {
+  '3개 일치': 0,
+  '4개 일치': 0,
+  '5개 일치': 0,
+  '5개 일치, 보너스 볼 일치': 0,
+  '6개 일치': 0,
+};
+
 class LotteryDraw {
-  userInput() {
-    let winningNumbers = {
-      winningNumber: [],
-      bonusNumer: 0,
-    };
+  userInput(lotto, price) {
     MissionUtils.Console.readLine('\n당첨 번호를 입력해 주세요.\n', (answer) => {
-      winningNumbers['winningNumber'] = answer.split(',').map((str) => parseInt(str));
+      WINNING_NUMBERS['winningNumber'] = answer.split(',').map((str) => parseInt(str));
       MissionUtils.Console.readLine('\n보너스 번호를 입력해 주세요.\n', (answer) => {
-        winningNumbers['bonusNumer'] = parseInt(answer);
-        MissionUtils.Console.close();
+        WINNING_NUMBERS['bonusNumer'] = parseInt(answer);
+        this.showResult(WINNING_NUMBERS, lotto, price);
       });
     });
-    return winningNumbers;
+  }
+
+  showResult(winningNumbers, lotto, price) {
+    lottoResult.compare(lotto, winningNumbers, WINNING_HISTORY);
+    lottoResult.printCompareResult(WINNING_HISTORY);
+    let profit = lottoResult.calcLottoReturn(WINNING_HISTORY, price);
+    lottoResult.printCalcResult(profit);
   }
 }
 
