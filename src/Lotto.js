3dfd085e686553855ff385d39de3eed621fafe55
yoorimseo <seou1225@naver.com>
2022-11-15 17:59:31 +0900

    feat(Lotto): 로또 번호의 유효성을 확인하는 기능 구현

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,16 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    this.duplication(numbers);
   }
 
   // TODO: 추가 기능 구현
+  duplication(numbers) {
+    let duplicationCheck = new Set(numbers);
+    if (duplicationCheck.size !== 6) {
+      throw Error('[ERROR] 로또 번호는 중복되지 않아야 합니다.');
+    }
+  }
 }
 
 module.exports = Lotto;
