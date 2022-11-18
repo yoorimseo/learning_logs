1c661657f1e3df8fdb1850ab8400d3d39acd4e52
yoorimseo <seou1225@naver.com>
2022-11-19 03:07:49 +0900

    feat: 게임 진행 상황과 결과를 출력하는 기능 구현

---
+++ src/view/OutputView.js
@@ -0,0 +1,37 @@
+// 제공된 OutputView 객체를 활용해 구현해야 한다.
+// OutputView의 파일 경로는 변경할 수 있다.
+// OutputView의 메서드의 이름은 변경할 수 없고, 인자는 필요에 따라 추가하거나 변경할 수 있다.
+// 값 출력을 위해 필요한 메서드를 추가할 수 있다.
+
+const { Console } = require('@woowacourse/mission-utils');
+
+/**
+ * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
+ */
+const OutputView = {
+  printGameStart() {
+    Console.print('다리 건너기 게임을 시작합니다./n');
+  },
+  /**
+   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
+   * <p>
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  printMap(currentBridge) {
+    currentBridge.forEach((bridge) => Console.print(bridge.join('')));
+  },
+
+  /**
+   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
+   * <p>
+   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+   */
+  printResult(finalBridge, successStatus, totalAttempts) {
+    Console.print('최종 게임 결과');
+    Console.print(finalBridge);
+    Console.print(`게임 성공 여부: ${successStatus}`);
+    Console.print(`총 시도한 횟수: ${totalAttempts}`);
+  },
+};
+
+module.exports = OutputView;
