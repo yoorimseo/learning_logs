a91bcb82cbbe4d1d3a3453655457baa1975e9b32
yoorimseo <seou1225@naver.com>
2022-11-18 23:36:12 +0900

    feat: 사용자로부터 값을 입력 받는 기능 구현

---
+++ src/view/InputView.js
@@ -0,0 +1,36 @@
+// 제공된 InputView 객체를 활용해 구현해야 한다.
+// InputView의 파일 경로는 변경할 수 있다.
+// InputView의 메서드의 인자는 변경할 수 있다.
+// 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
+
+const { Console } = require('@woowacourse/mission-utils');
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+const InputView = {
+  /**
+   * 다리의 길이를 입력받는다.
+   */
+  readBridgeSize(question, callback) {
+    Console.readLine(question, callback);
+  },
+
+  /**
+   * 사용자가 이동할 칸을 입력받는다.
+   */
+  readMoving(question, callback) {
+    Console.readLine(question, callback);
+  },
+
+  /**
+   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+   */
+  readGameCommand(question, callback) {
+    Console.readLine(question, callback);
+  },
+};
+
+module.exports = InputView;
+
+InputView.readBridgeSize();
