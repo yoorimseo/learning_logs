238ad6b3ea36a34019cb5ba226b2925908e9459f
yoorimseo <seou1225@naver.com>
2022-11-22 12:35:58 +0900

    test: 다리의 길이 입력값 유효성을 검사하는 테스트 구현

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,15 @@
+const App = require('../src/App');
+const app = new App();
+
+describe('사용자 입력 테스트', () => {
+  test.each([['abc'], ['~!@'], ['가나다']])('숫자가 아닌 경우에 대한 예외 처리', (input) => {
+    expect((input) => {
+      app.checkBridgeLengthValidation(input);
+    }).toThrow('[ERROR]');
+  });
+  test.each([['0'], ['21']])('3 미만 20 초과의 숫자일 경우에 대한 예외 처리', (input) => {
+    expect((input) => {
+      app.checkBridgeLengthValidation(input);
+    }).toThrow('[ERROR]');
+  });
+});
