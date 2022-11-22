a4c2a3c528735bc289e6e56631e8e46c3b71634d
yoorimseo <seou1225@naver.com>
2022-11-22 12:52:45 +0900

    test: 게임 종료 문구 입력값 유효성을 검사하는 테스트 구현

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -12,9 +12,10 @@ describe('사용자 입력 테스트', () => {
       app.checkBridgeLengthValidation(input);
     }).toThrow('[ERROR]');
   });
-  test.each([['0'], ['A'], ['a'], ['!'], ['ㅁ']])('U와 D가 아닐 경우에 대한 예외 처리', (input) => {
+  test.each([['0'], ['A'], ['a'], ['!'], ['ㅁ']])('U와 D / R과 Q가 아닐 경우에 대한 예외 처리', (input) => {
     expect((input) => {
       app.checkMoveBlockValidation(input);
+      app.checkGameCommandValidation(input);
     }).toThrow('[ERROR]');
   });
 });
