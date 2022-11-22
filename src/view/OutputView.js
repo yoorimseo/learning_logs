668154b9be93450566ea8cae0ecc3a86851e114a
yoorimseo <seou1225@naver.com>
2022-11-22 14:27:35 +0900

    feat: 에러 메시지 출력을 담당하는 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -8,6 +8,10 @@ const OutputView = {
     Console.print('다리 건너기 게임을 시작합니다.');
   },
 
+  printError(errorMessage) {
+    Console.print(errorMessage);
+  },
+
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
