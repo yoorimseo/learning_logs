45c4b205045b60c60517b1ceca0ac050fb420024
yoorimseo <seou1225@naver.com>
2022-11-22 13:36:11 +0900

    style: 다리 길이 입력 문구 수정

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -5,7 +5,7 @@ const { Console } = require('@woowacourse/mission-utils');
  */
 const OutputView = {
   printGameStart() {
-    Console.print('다리 건너기 게임을 시작합니다./n');
+    Console.print('다리 건너기 게임을 시작합니다.');
   },
 
   /**
