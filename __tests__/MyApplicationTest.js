008825c67b6e03bae43e45d745894c43261edf70
yoorimseo <seou1225@naver.com>
2022-11-22 16:26:19 +0900

    style: 사용하지 않는 코드 제거 및 파일 경로 수정

--- __tests__/MyApplicationTest.js
+++ __tests__/MyApplicationTest.js
@@ -27,16 +27,6 @@ const getOutput = (logSpy) => {
   return [...logSpy.mock.calls].join('');
 };
 
-const runException = (inputs) => {
-  mockQuestions(inputs);
-  const logSpy = getLogSpy();
-  const app = new App();
-
-  app.play();
-
-  expectLogContains(getOutput(logSpy), ['[ERROR]']);
-};
-
 const expectLogContains = (received, logs) => {
   logs.forEach((log) => {
     expect(received).toEqual(expect.stringContaining(log));
--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,21 +1,20 @@
-const App = require('../src/App');
-const app = new App();
+const BridgeGameController = require('../src/controller/BridgeGameController');
 
 describe('사용자 입력 테스트', () => {
-  test.each([['abc'], ['~!@'], ['가나다']])('숫자가 아닌 경우에 대한 예외 처리', (input) => {
+  test.each([['abc'], ['~!@'], ['가나다']])('숫자가 아닌 경우에 대한 예외 처리', () => {
     expect((input) => {
-      app.checkBridgeLengthValidation(input);
+      BridgeGameController.checkBridgeLengthValidation(input);
     }).toThrow('[ERROR]');
   });
-  test.each([['0'], ['21']])('3 미만 20 초과의 숫자일 경우에 대한 예외 처리', (input) => {
+  test.each([['0'], ['21']])('3 미만 20 초과의 숫자일 경우에 대한 예외 처리', () => {
     expect((input) => {
-      app.checkBridgeLengthValidation(input);
+      BridgeGameController.checkBridgeLengthValidation(input);
     }).toThrow('[ERROR]');
   });
-  test.each([['0'], ['A'], ['a'], ['!'], ['ㅁ']])('U와 D / R과 Q가 아닐 경우에 대한 예외 처리', (input) => {
+  test.each([['0'], ['A'], ['a'], ['!'], ['ㅁ']])('U와 D / R과 Q가 아닐 경우에 대한 예외 처리', () => {
     expect((input) => {
-      app.checkMoveBlockValidation(input);
-      app.checkGameCommandValidation(input);
+      BridgeGameController.checkMoveBlockValidation(input);
+      BridgeGameController.checkGameCommandValidation(input);
     }).toThrow('[ERROR]');
   });
 });
