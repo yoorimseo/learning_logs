5845f7be6ab61490693c5a11ed496afd279cde71
yoorimseo <seou1225@naver.com>
2022-11-27 02:06:27 +0900

    feat: 사용자 입력값 예외처리

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const app = document.querySelector('.app');
+const inpTodo = app.querySelector('#inp-todo');
+const btnAdd = app.querySelector('.btn-add');
+const list = app.querySelector('.list');
+
+// 1. input창에 할 일을 입력할 수 있어야 한다.
+
+// 2. 추가하기 버튼을 클릭하면, 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+btnAdd.addEventListener('click', () => {
+  // 1-1. 사용자가 할 일을 입력하지 않으면, '할 일을 입력해주세요.'라는 경고창을 띄운다.
+  if (inpTodo.value === '') {
+    alert('할 일을 입력해주세요.');
+  }
+
+  // 1-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+
+  // 1-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
+});
+
+// 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
+
+// 4. 삭제하기 버튼을 클릭하면, 해당 리스트가 삭제되어야 한다.
