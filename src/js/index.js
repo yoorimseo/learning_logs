0deaa153b671a24ba5dbaec182c2be8b4468f8b1
yoorimseo <seou1225@naver.com>
2022-11-27 02:16:33 +0900

    style: 주석 내용 수정

--- src/js/index.js
+++ src/js/index.js
@@ -9,19 +9,19 @@ const btnDelete = document.createElement('button');
 
 // 2. 추가하기 버튼을 클릭하면, 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
 btnAdd.addEventListener('click', () => {
-  // 1-1. 사용자가 할 일을 입력하지 않으면, '할 일을 입력해주세요.'라는 경고창을 띄운다.
+  // 2-1. 사용자가 할 일을 입력하지 않으면, '할 일을 입력해주세요.'라는 경고창을 띄운다.
   if (inpTodo.value === '') {
     alert('할 일을 입력해주세요.');
   }
 
-  // 1-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+  // 2-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
   liEl.innerText = inpTodo.value;
   liEl.classList.add('todo');
   btnDelete.innerText = '삭제하기';
   list.appendChild(liEl);
   liEl.appendChild(btnDelete);
 
-  // 1-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
+  // 2-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
   inpTodo.value = '';
 });
 
