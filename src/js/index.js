5446ad01eb49821a3da0a845198bdd8aeb3e8097
yoorimseo <seou1225@naver.com>
2022-11-27 02:14:34 +0900

    feat: 할 일을 추가하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ const app = document.querySelector('.app');
 const inpTodo = app.querySelector('#inp-todo');
 const btnAdd = app.querySelector('.btn-add');
 const list = app.querySelector('.list');
+const liEl = document.createElement('li');
+const btnDelete = document.createElement('button');
 
 // 1. input창에 할 일을 입력할 수 있어야 한다.
 
@@ -13,8 +15,14 @@ btnAdd.addEventListener('click', () => {
   }
 
   // 1-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+  liEl.innerText = inpTodo.value;
+  liEl.classList.add('todo');
+  btnDelete.innerText = '삭제하기';
+  list.appendChild(liEl);
+  liEl.appendChild(btnDelete);
 
   // 1-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
+  inpTodo.value = '';
 });
 
 // 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
