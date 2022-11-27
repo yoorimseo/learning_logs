ccdfe276af53b958b34f4803ad606e804d08e18d
yoorimseo <seou1225@naver.com>
2022-11-27 22:12:15 +0900

    fix: 리스트가 두개 이상 추가되지 않는 오류 수정

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,6 @@ const app = document.querySelector('.app');
 const inpTodo = app.querySelector('#inp-todo');
 const btnAdd = app.querySelector('.btn-add');
 const list = app.querySelector('.list');
-const liEl = document.createElement('li');
-const btnDelete = document.createElement('button');
 
 // 1. input창에 할 일을 입력할 수 있어야 한다.
 
@@ -14,6 +12,9 @@ btnAdd.addEventListener('click', () => {
     alert('할 일을 입력해주세요.');
   } else {
     // 2-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+    const liEl = document.createElement('li');
+    const btnDelete = document.createElement('button');
+
     liEl.innerText = inpTodo.value;
     liEl.classList.add('todo');
     btnDelete.innerText = '삭제하기';
@@ -22,15 +23,15 @@ btnAdd.addEventListener('click', () => {
 
     // 2-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
     inpTodo.value = '';
+
+    // 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
+    liEl.addEventListener('click', () => {
+      liEl.classList.toggle('done');
+    });
+
+    // 4. 삭제하기 버튼을 클릭하면, 해당 리스트가 삭제되어야 한다.
+    btnDelete.addEventListener('click', () => {
+      liEl.remove();
+    });
   }
-});
-
-// 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
-liEl.addEventListener('click', () => {
-  liEl.classList.toggle('done');
-});
-
-// 4. 삭제하기 버튼을 클릭하면, 해당 리스트가 삭제되어야 한다.
-btnDelete.addEventListener('click', () => {
-  liEl.remove();
 });
