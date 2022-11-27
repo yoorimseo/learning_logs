5b4f028f294da8ed09369177a9bb811ec1fdbb24
yoorimseo <seou1225@naver.com>
2022-11-27 22:43:15 +0900

    feat: 사용자가 입력한 값을 로컬스토리지에 저장하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@ const app = document.querySelector('.app');
 const inpTodo = app.querySelector('#inp-todo');
 const btnAdd = app.querySelector('.btn-add');
 const list = app.querySelector('.list');
+const todoList = [];
+
+// 0. 새로고침 하면 로컬스토리지에 있는 내 투두 리스트가 불러와져야 한다.
 
 // 1. input창에 할 일을 입력할 수 있어야 한다.
 
@@ -15,13 +18,13 @@ btnAdd.addEventListener('click', () => {
     const liEl = document.createElement('li');
     const btnDelete = document.createElement('button');
 
-    liEl.innerText = inpTodo.value;
-    liEl.classList.add('todo');
-    btnDelete.innerText = '삭제하기';
-    list.appendChild(liEl);
-    liEl.appendChild(btnDelete);
+    // 2-3. 사용자가 입력한 할 일을 로컬 스토리지에 저장한다.
+    todoList.push(inpTodo.value);
+    todoList.forEach((item, index) => {
+      window.localStorage.setItem(index, item);
+    });
 
-    // 2-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
+    // 2-4. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
     inpTodo.value = '';
 
     // 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
