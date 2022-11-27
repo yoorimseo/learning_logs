0465126a3bd412aef29ffe56776a861c13fdd4f2
yoorimseo <seou1225@naver.com>
2022-11-27 21:37:10 +0900

    fix: 투두리스트 입력창이 빈 값일 때, 경고창 띄운 후 빈값 리스트가 추가되는 오류 수정

--- src/js/index.js
+++ src/js/index.js
@@ -12,17 +12,17 @@ btnAdd.addEventListener('click', () => {
   // 2-1. 사용자가 할 일을 입력하지 않으면, '할 일을 입력해주세요.'라는 경고창을 띄운다.
   if (inpTodo.value === '') {
     alert('할 일을 입력해주세요.');
+  } else {
+    // 2-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
+    liEl.innerText = inpTodo.value;
+    liEl.classList.add('todo');
+    btnDelete.innerText = '삭제하기';
+    list.appendChild(liEl);
+    liEl.appendChild(btnDelete);
+
+    // 2-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
+    inpTodo.value = '';
   }
-
-  // 2-2. 하단에 빈 박스 아이콘과 할 일의 내용, 그리고 삭제하기 버튼이 있는 리스트 하나가 추가되어야 한다.
-  liEl.innerText = inpTodo.value;
-  liEl.classList.add('todo');
-  btnDelete.innerText = '삭제하기';
-  list.appendChild(liEl);
-  liEl.appendChild(btnDelete);
-
-  // 2-3. 할 일이 추가되면, input창에 빈 내용으로 바뀌어야 한다.
-  inpTodo.value = '';
 });
 
 // 3. 할 일을 완료하고 해당 리스트를 클릭하면, 할 일을 완료했다는 의미의 체크박스 아이콘과 취소선이 생겨야 한다.
