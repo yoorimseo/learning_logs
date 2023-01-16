730768e65773b9298fc40a81990d786075370d63
yoorimseo <seou1225@naver.com>
2023-01-16 19:17:30 +0900

    init: 스타일 컴포넌트 설치 및 초기 세팅 (#1)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
 import Router from './routes/Router';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/Theme';
 
 function App() {
   return (
     <div className='App'>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <Reset />
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
     </div>
   );
 }
