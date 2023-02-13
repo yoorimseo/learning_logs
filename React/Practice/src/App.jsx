d52e5a10ef665faebde54ebf64ce06e781b6ec92
yoorimseo <seou1225@naver.com>
2023-02-13 15:10:02 +0900

    프로젝트 폴더 이름 변경

---
+++ React/Practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InputSample from './InputSample';
+
+function App() {
+  return (
+    <div className='App'>
+      <InputSample />
+    </div>
+  );
+}
+
+export default App;
