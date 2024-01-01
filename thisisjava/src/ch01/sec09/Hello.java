0efec684c5968393cf7102a3db0da71a73415b3a
yoorimseo <seou1225@naver.com>
2024-01-01 21:55:54 +0900

    1-1. 자바 시작하기

---
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,16 @@
+package ch01.sec09;
+/**
+ * @author yoorimseo
+*/
+/*
+ * 장제목: 1장 자바 시작하기
+ * 작성일: 2024.01.01
+*/
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+
+}
