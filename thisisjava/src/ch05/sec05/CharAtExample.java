af8556a03f4ba1bf31b9de0b54f04ef1f7994328
yoorimseo <seou1225@naver.com>
2024-01-11 03:28:14 +0900

    2-1. 참조 타입

---
+++ thisisjava/src/ch05/sec05/CharAtExample.java
@@ -0,0 +1,19 @@
+package ch05.sec05;
+
+public class CharAtExample {
+	public static void main(String[] args) {
+		String ssn = "9912252345678";
+		char sex = ssn.charAt(6);
+		
+		switch(sex) {
+		case '1':
+		case '3':
+			System.out.println("남자입니다.");
+			break;
+		case '2':
+		case '4':
+			System.out.println("여자입니다.");
+			break;
+		}
+	}
+}
---
+++ thisisjava/src/ch05/sec05/EmptyStringExample.java
@@ -0,0 +1,11 @@
+package ch05.sec05;
+
+public class EmptyStringExample {
+	public static void main(String[] args) {
+		String hobby = "";
+		
+		if(hobby.equals("")) {
+			System.out.println("hobby 변수가 참조하는 String 갹체는 빈 문자열");
+		}
+	}
+}
