d46ead4c448370c3cf2e21f0e83515ae45833377
yoorimseo <seou1225@naver.com>
2024-07-03 21:17:05 +0900

    feat: todo 추가 기능 함수 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const todoForm = document.querySelector('#todoForm');
+const todoList = document.querySelector('#todoList');
 
 todoForm.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -8,4 +9,14 @@ todoForm.addEventListener('submit', function (e) {
   if (todoInput.value === '') {
     alert('할 일을 입력해주세요.');
   }
+
+  addTodoItem(todoInput);
 });
+
+function addTodoItem(input) {
+  const todoItem = document.createElement('li');
+  todoItem.innerText = input.value;
+
+  todoList.append(todoItem);
+  input.value = '';
+}
