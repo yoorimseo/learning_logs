e1edbc6fa5ef6c99bc3c63538a23efe21837513b
yoorimseo <seou1225@naver.com>
2024-07-19 21:39:01 +0900

    feat: while문을 사용하여 게임 playAgain 상태에 따라 게임 재시작 혹은 종료될 수 있도록 구현

--- practice/freecourse/01_baseball/src/App.js
+++ practice/freecourse/01_baseball/src/App.js
@@ -4,6 +4,7 @@ import User from './User.js';
 
 class App {
   constructor() {
+    this.playAgain = true;
     this.computerNumbers = null;
     this.userNumbers = null;
     this.strikes = 0;
@@ -14,30 +15,35 @@ class App {
     // 게임 시작 문구 출력
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
-    // 랜덤한 3개의 숫자 생성
-    const computer = new Computer();
-    this.computerNumbers = computer.generateRandomNumber();
-    console.log(this.computerNumbers);
+    while (this.playAgain) {
+      // 랜덤한 3개의 숫자 생성
+      const computer = new Computer();
+      this.computerNumbers = computer.generateRandomNumber();
+      console.log(this.computerNumbers);
 
-    // 컴퓨터의 값과 사용자 입력값이 동일할 때까지 비교하여 계산 결과 출력
-    while (true) {
-      // 사용자로부터 서로 다른 3개의 숫자를 입력받음
       const user = new User();
-      this.userNumbers = await user.getUserNumber();
 
-      const [strikes, balls] = computer.calculateScore(this.computerNumbers, this.userNumbers);
-      this.strikes = strikes;
-      this.balls = balls;
+      // 사용자와의 게임 루프
+      while (this.strikes < 3) {
+        // 사용자로부터 서로 다른 3개의 숫자를 입력받음
+        this.userNumbers = await user.getUserNumber();
 
-      computer.printScore(this.strikes, this.balls);
+        const [strikes, balls] = computer.calculateScore(this.computerNumbers, this.userNumbers);
+        this.strikes = strikes;
+        this.balls = balls;
 
-      if (this.strikes === 3) {
-        console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        const restart = await user.askForRestart();
-        console.log(restart);
-        break;
+        computer.printScore(this.strikes, this.balls);
+
+        if (this.strikes === 3) {
+          console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+          break;
+        }
       }
+
+      // 게임 재시작 여부 확인
+      this.playAgain = await user.askForRestart();
     }
+    console.log('게임을 종료합니다.');
   }
 }
 
