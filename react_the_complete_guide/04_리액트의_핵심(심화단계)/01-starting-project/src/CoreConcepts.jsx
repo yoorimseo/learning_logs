21f0cda1898e3ed56103c06a3b251a3a3a981f11
yoorimseo <seou1225@naver.com>
2024-08-16 23:04:25 +0900

    Section 4: 리액트의 핵심 - 심화 단계(1)

---
+++ react_the_complete_guide/04_리액트의_핵심(심화단계)/01-starting-project/src/CoreConcepts.jsx
@@ -0,0 +1,19 @@
+import CoreConcept from './components/CoreConcept.jsx';
+
+import { CORE_CONCEPTS } from './data';
+
+export default function CoreConcepts() {
+  return (
+    <section id='core-concepts'>
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept
+            key={conceptItem.title}
+            {...conceptItem}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+}
