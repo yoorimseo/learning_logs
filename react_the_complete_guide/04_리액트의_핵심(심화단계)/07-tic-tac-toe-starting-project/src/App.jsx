61a2dfffd3eb51701337e7165e89ffbafd33a4e1
yoorimseo <seou1225@naver.com>
2024-08-19 20:55:17 +0900

    Section 4: 리액트의 핵심 - 심화 단계(2)

---
+++ react_the_complete_guide/04_리액트의_핵심(심화단계)/07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,24 @@
+import Player from './components/Player.jsx';
+
+function App() {
+  return (
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <Player
+            name='Player 1'
+            symbol='X'
+          />
+          <Player
+            name='Player 2'
+            symbol='O'
+          />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
+}
+
+export default App;
