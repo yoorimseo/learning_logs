a23db75f6895c5ff339304cb4c7b87f855a74e4a
yoorimseo <seou1225@naver.com>
2024-08-25 12:28:33 +0900

    Section 12: 연습 프로젝트: 퀴즈 앱 만들기

---
+++ react_the_complete_guide/12_연습프로젝트(퀴즈_앱)/01-starting-project/src/components/Question.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+import Answers from './Answers';
+import QuestionTimer from './QuestionTimer';
+
+import QUESTIONS from '../questions.js';
+
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = '';
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
+  return (
+    <div id='question'>
+      {/* 여기서 key={activeQuestionIndex} 처럼 같은 key 속성의 값을 두 개 이상의 다른 형제 컴포넌트에서 사용해서는 안됨
+      -> 별도의 컴포넌트를 새로 만들어서 해당 key 속성을 삭제해서 오류 해결 가능
+      */}
+      <QuestionTimer
+        // key={activeQuestionIndex}
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+        mode={answerState}
+      />
+      <h2>{QUESTIONS[index].text}</h2>
+      <Answers
+        // key={activeQuestionIndex}
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
